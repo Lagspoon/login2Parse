ea3202ab17e1b8c67f79ff36c062737161b9a1df
Lagspoon <lagspoon@gmail.com>
2013-11-06 22:56:12 +0100

    Initial Commit

---
+++ loginMenu/LOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LOAppDelegate.h
+//  loginMenu
+//
+//  Created by Olivier Delecueillerie on 06/11/2013.
+//  Copyright (c) 2013 Olivier Delecueillerie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ loginMenu/LOViewController.h
@@ -0,0 +1,13 @@
+//
+//  LOViewController.h
+//  loginMenu
+//
+//  Created by Olivier Delecueillerie on 06/11/2013.
+//  Copyright (c) 2013 Olivier Delecueillerie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LOViewController : UIViewController
+
+@end
